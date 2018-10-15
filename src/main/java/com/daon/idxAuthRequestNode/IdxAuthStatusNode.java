2d4cbedcbde9d74c6d4bbabbcbd46fb5391cc365
Frank Gasparovic <frank.gasparovic@forgerock.com>
2018-10-15 08:58:36 -0700

    Proposed Changes

--- src/main/java/com/daon/idxAuthRequestNode/IdxAuthStatusNode.java
+++ src/main/java/com/daon/idxAuthRequestNode/IdxAuthStatusNode.java
@@ -17,17 +17,14 @@
 
 package com.daon.idxAuthRequestNode;
 
+import static com.daon.idxAuthRequestNode.IdxCommon.getTenantRepoFactory;
 import static org.forgerock.openam.auth.node.api.Action.goTo;
 import static org.forgerock.openam.auth.node.api.SharedStateConstants.USERNAME;
 
 import com.daon.identityx.rest.model.pojo.AuthenticationRequest;
 import com.identityx.clientSDK.TenantRepoFactory;
-import com.identityx.clientSDK.credentialsProviders.EncryptedKeyPropFileCredentialsProvider;
 import com.identityx.clientSDK.exceptions.IdxRestException;
 import com.identityx.clientSDK.repositories.AuthenticationRequestRepository;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
 import java.util.List;
 import javax.inject.Inject;
 import org.forgerock.guava.common.collect.ImmutableList;
@@ -55,8 +52,6 @@ public class IdxAuthStatusNode implements Node {
      * Configuration for the node.
      */
     interface Config {
-
-
     }
 
 
@@ -70,86 +65,50 @@ public class IdxAuthStatusNode implements Node {
     @Override
     public Action process(TreeContext context) throws NodeProcessException {
         String username = context.sharedState.get(SharedStateConstants.USERNAME).asString();
-        TenantRepoFactory tenantRepoFactory = null;
-        try {
-            //Pull these config values from SharedState. These are set in the IdxCheckEnrollmentStatus node
-            String pathToKeyStore = context.sharedState.get("IdxPathToKeyStore").asString();
-            if (pathToKeyStore == null) {
-                logger.error("Error: Path to JKS KeyStore not found in SharedState!");
-                throw new NodeProcessException("Path to JKS KeyStore not found!");
-            }
-            InputStream keyStore = new FileInputStream(new File(pathToKeyStore));
 
-            String pathToCredentialProperties = context.sharedState.get("IdxPathToCredentialProperties").asString();
-            if (pathToCredentialProperties == null) {
-                logger.error("Error: Path to credential.properties file not found in SharedState!");
-                throw new NodeProcessException("Path to credential.properties file not found!");
-            }
-            InputStream credentialsProperties = new FileInputStream(new File(pathToCredentialProperties));
-
-            String jksPassword = context.sharedState.get("IdxJksPassword").asString();
-            if (jksPassword == null) {
-                logger.error("Error: JKS Password not found in SharedState!");
-                throw new NodeProcessException("JKS password not found in SharedState!");
-            }
-            String keyAlias = context.sharedState.get("IdxKeyAlias").asString();
-            if (keyAlias == null) {
-                logger.error("Error: Key Alias not found in SharedState!");
-                throw new NodeProcessException("Key Alias not found in SharedState!");
-            }
-            String keyPassword = context.sharedState.get("IdxKeyPassword").asString();
-            if (keyPassword == null) {
-                logger.error("Error: Key Password not found in SharedState!");
-                throw new NodeProcessException("Key password not found in SharedState!");
-            }
-            EncryptedKeyPropFileCredentialsProvider provider = new EncryptedKeyPropFileCredentialsProvider(keyStore,
-                    jksPassword, credentialsProperties, keyAlias, keyPassword);
-
-             tenantRepoFactory = new TenantRepoFactory(provider);
-             logger.debug("Connected to the IdentityX Server");
-        } catch (Exception ex) {
-            logger.error("An exception occurred connecting to the IX Server: " + ex );
-            throw new NodeProcessException("Error creating tenant factory" + ex);
-        }
+        TenantRepoFactory tenantRepoFactory = getTenantRepoFactory(context);
+        logger.debug("Connected to the IdentityX Server");
 
 
-    //call API to check status. Return true, false or pending
-    //get the authHref value from sharedState
+        //call API to check status. Return true, false or pending
+        //get the authHref value from sharedState
         String IDX_HREF_KEY = "idx-auth-ref-shared-state-key";
         String authHref = context.sharedState.get(IDX_HREF_KEY).asString();
-      if (authHref == null) {
-        logger.error("Error: href not found in SharedState!");
-        throw new NodeProcessException("Unable to authenticate - HREF not found!");
-      }
+        if (authHref == null) {
+            logger.error("Error: href not found in SharedState!");
+            throw new NodeProcessException("Unable to authenticate - HREF not found!");
+        }
 
-      String status = getAuthenticationRequestStatus(authHref, tenantRepoFactory);
-      if(status.equalsIgnoreCase("COMPLETED_SUCCESSFUL")) {
-          return goTo(SUCCESS).replaceSharedState(context.sharedState.copy().put(USERNAME, username)).build();
-      }
-      else if (status.equalsIgnoreCase("PENDING")) {
-          return goTo(PENDING).replaceSharedState(context.sharedState.copy().put(USERNAME, username)).build();
-      }
-      else if (status.equalsIgnoreCase("EXPIRED")) {
-          return goTo(EXPIRED).replaceSharedState(context.sharedState.copy().put(USERNAME, username)).build();
-      }
-      else {
-         return goTo(FAILED).build();
-      }
-
+        String status = getAuthenticationRequestStatus(authHref, tenantRepoFactory);
+        if(status.equalsIgnoreCase("COMPLETED_SUCCESSFUL")) {
+            return goTo(SUCCESS).replaceSharedState(context.sharedState.copy().put(USERNAME, username)).build();
+        }
+        else if (status.equalsIgnoreCase("PENDING")) {
+            return goTo(PENDING).replaceSharedState(context.sharedState.copy().put(USERNAME, username)).build();
+        }
+        else if (status.equalsIgnoreCase("EXPIRED")) {
+            return goTo(EXPIRED).replaceSharedState(context.sharedState.copy().put(USERNAME, username)).build();
+        }
+        else {
+            return goTo(FAILED).build();
+        }
     }
 
     private String getAuthenticationRequestStatus(String authRequestHref, TenantRepoFactory tenantRepoFactory) throws
             NodeProcessException {
+
+        AuthenticationRequestRepository authenticationRequestRepo = tenantRepoFactory.getAuthenticationRequestRepo();
+
+        AuthenticationRequest request;
         try {
-            AuthenticationRequest request;
-            AuthenticationRequestRepository authenticationRequestRepo = tenantRepoFactory.getAuthenticationRequestRepo();
             request = authenticationRequestRepo.get(authRequestHref);
-            logger.debug("Retrieving an AuthenticationRequest with an HREF of " + authRequestHref);
-            return request.getStatus().toString();
-        } catch (IdxRestException ex) {
-            logger.debug("An exception occurred while attempting to determine the status of the authentication request.  Exception: " + ex.getMessage());
-            throw new NodeProcessException(ex);
+        } catch (IdxRestException e) {
+            logger.debug("An exception occurred while attempting to determine the status of the authentication " +
+                    "request.  Exception: " + e.getMessage());
+            throw new NodeProcessException(e);
         }
+        logger.debug("Retrieving an AuthenticationRequest with an HREF of " + authRequestHref);
+        return request.getStatus().toString();
 	}
 
 
