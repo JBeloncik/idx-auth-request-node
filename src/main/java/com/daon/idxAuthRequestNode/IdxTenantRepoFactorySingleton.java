96bd4450896ca9079480b7011ede6636d4ba8d7d
Frank Gasparovic <frank.gasparovic@forgerock.com>
2019-04-25 13:01:58 -0700

    Adding more logging for IdxTeantRepoFactory creation

--- src/main/java/com/daon/idxAuthRequestNode/IdxTenantRepoFactorySingleton.java
+++ src/main/java/com/daon/idxAuthRequestNode/IdxTenantRepoFactorySingleton.java
@@ -1,5 +1,7 @@
 package com.daon.idxAuthRequestNode;
 
+import org.forgerock.openam.auth.node.api.NodeProcessException;
+
 import com.identityx.clientSDK.TenantRepoFactory;
 import com.identityx.clientSDK.credentialsProviders.EncryptedKeyPropFileCredentialsProvider;
 import com.identityx.clientSDK.def.ICredentialsProvider;
@@ -17,32 +19,34 @@ class IdxTenantRepoFactorySingleton {
     TenantRepoFactory tenantRepoFactory;
 
     private IdxTenantRepoFactorySingleton(String keyStorePath, String jksPassword, String credentialPropertiesPath,
-                                String keyAlias, String keyPass) throws IdxRestException
-    {
+                                String keyAlias, String keyPass) throws NodeProcessException {
+        InputStream keyStore;
+        InputStream credentialProperties;
         try {
-
-            InputStream keyStore = new FileInputStream(new File(keyStorePath));
-            InputStream credentialProperties = new FileInputStream(new File(credentialPropertiesPath));
-
-            ICredentialsProvider tenantCredentialProvider = new EncryptedKeyPropFileCredentialsProvider(keyStore,
-                    jksPassword, credentialProperties, keyAlias, keyPass);
-
-            tenantRepoFactory = new TenantRepoFactory(tenantCredentialProvider);
-
-        } catch (IdxRestException e) {
-            e.printStackTrace();
-        } catch (ClientInitializationException e) {
-            e.printStackTrace();
+            keyStore = new FileInputStream(new File(keyStorePath));
         } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            throw new NodeProcessException("Unable to find keystore file at: " + keyStorePath, e);
+        }
+        try {
+            credentialProperties = new FileInputStream(new File(credentialPropertiesPath));
+        } catch (FileNotFoundException e) {
+            throw new NodeProcessException("Unable to find credential properties file at: " + credentialPropertiesPath,
+                                           e);
+        }
+        try {
+            tenantRepoFactory = new TenantRepoFactory(new EncryptedKeyPropFileCredentialsProvider(keyStore,
+                                                                                                  jksPassword,
+                                                                                                  credentialProperties,
+                                                                                                  keyAlias, keyPass));
+        } catch (IdxRestException | ClientInitializationException e) {
+            throw new NodeProcessException(e);
         }
     }
 
     static IdxTenantRepoFactorySingleton getInstance(String keyStorePath, String jksPassword,
-                                                     String credentialPropertiesPath, String keyAlias, String keyPass) throws IdxRestException
-    {
-        if(tenantRepoInstance == null)
-        {
+                                                     String credentialPropertiesPath, String keyAlias,
+                                                     String keyPass) throws NodeProcessException {
+        if(tenantRepoInstance == null) {
             tenantRepoInstance = new IdxTenantRepoFactorySingleton(keyStorePath, jksPassword, credentialPropertiesPath,
                     keyAlias, keyPass);
         }
